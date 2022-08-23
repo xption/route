172091060bde55c9b2a6d5fa4c78d91fb1063fb3
liuxianpeng <z@xption.com>
2022-08-23 21:26:31 +0800

    基础的A*算法

---
+++ src/1.A-Star/RouteManager.ts
@@ -0,0 +1,138 @@
+import { Cell } from "./Cell";
+import { MapData } from "./MapData";
+import { MN } from "./MN";
+
+export class RouteManager {
+    private static AroundPosArray: MN[] = [ new MN(0, 1), new MN(1, 1), new MN(1, 0), new MN(1, -1), 
+                                            new MN(0, -1), new MN(-1, -1), new MN(-1, 0), new MN(-1, 1)];
+
+    public static Route(mapData: MapData, cellFrom: Cell, cellTo: Cell): void {
+        let openMap: Map<string, Cell> = new Map<string, Cell>();
+        openMap.set(cellFrom.getKey(), cellFrom);
+
+        let closeSet: Set<string> = new Set<string>();
+
+        while (openMap.size > 0) {
+            let minFCell: Cell = RouteManager.FindMinFCell(openMap);
+            if (minFCell == null) {
+                break;
+            }
+
+            openMap.delete(minFCell.getKey());
+            closeSet.add(minFCell.getKey());
+
+            let aroundCellArray: Cell[] = RouteManager.FindAroundCellArray(minFCell, cellTo, mapData);
+            for (let i = 0; i < aroundCellArray.length; i++) {
+                let aroundCell: Cell = aroundCellArray[i];
+                if (closeSet.has(aroundCell.getKey())) {
+                    continue;
+                }
+
+                let cellExist: Cell = openMap.get(aroundCell.getKey());
+                if (!cellExist) {
+                    openMap.set(aroundCell.getKey(), aroundCell);
+                } else if (cellExist.g > aroundCell.g) {
+                    cellExist.g = aroundCell.g;
+                    cellExist.prev = minFCell;
+                }
+            }
+
+            // 判断是否到达
+            let checkCell: Cell = openMap.get(cellTo.getKey());
+            if (checkCell) {
+                cellTo = checkCell;
+                break;
+            }
+        }
+
+        if (!cellTo.prev) {
+            console.log('无法到达');
+            return;
+        }
+
+        // while (cellTo.prev) {
+        //     console.log(cellTo.m, cellTo.n);
+        //     cellTo = cellTo.prev;
+        // }
+    }
+
+    private static FindMinFCell(cellMap: Map<string, Cell>): Cell {
+        let minFCell: Cell = null;
+        let minF: number = Number.MAX_VALUE;
+
+        cellMap.forEach((cell, key) => {
+            if (cell.getF() < minF) {
+                minF = cell.getF();
+                minFCell = cell;
+            }
+        });
+
+        return minFCell;
+    }
+
+    private static FindAroundCellArray(centerCell: Cell, toCell: Cell, mapData: MapData): Cell[] {
+        let aroundCellArray: Cell[] = [];
+
+        let centerM: number = centerCell.m;
+        let centerN: number = centerCell.n;
+
+        let isTopThrough    = !mapData.isBlockedMN(centerM + RouteManager.AroundPosArray[0].m, centerN + RouteManager.AroundPosArray[0].n);
+        let isRightThrough  = !mapData.isBlockedMN(centerM + RouteManager.AroundPosArray[2].m, centerN + RouteManager.AroundPosArray[2].n);
+        let isBottomThrough = !mapData.isBlockedMN(centerM + RouteManager.AroundPosArray[4].m, centerN + RouteManager.AroundPosArray[4].n);
+        let isLeftThrough   = !mapData.isBlockedMN(centerM + RouteManager.AroundPosArray[6].m, centerN + RouteManager.AroundPosArray[6].n);
+
+        for (let i = 0; i < 8; i++) {
+            let m: number = centerM + RouteManager.AroundPosArray[i].m;
+            let n: number = centerN + RouteManager.AroundPosArray[i].n;
+            if (mapData.isBlockedMN(m, n)) {
+                continue;
+            }
+
+            let canThrough: boolean = true;
+
+            switch (i) {
+                case 1:
+                    canThrough = isTopThrough && isRightThrough;
+                    break;
+
+                case 3:
+                    canThrough = isRightThrough && isBottomThrough;
+                    break;
+
+                case 5:
+                    canThrough = isBottomThrough && isLeftThrough;
+                    break;
+
+                case 7:
+                    canThrough = isLeftThrough && isTopThrough;
+                    break;
+                
+                default:
+                    break;
+            }
+
+            if (canThrough) {
+                let cell: Cell = new Cell(0, 0);
+                cell.m = m;
+                cell.n = n;
+                cell.h = RouteManager.calcH(cell, toCell);
+                cell.prev = centerCell;
+
+                if (Math.abs(RouteManager.AroundPosArray[i].m) + Math.abs(RouteManager.AroundPosArray[i].n) == 1) {
+                    cell.g = centerCell.g + 10;
+                }
+                else {
+                    cell.g = centerCell.g + 14;
+                }
+
+                aroundCellArray.push(cell);
+            }
+        }
+
+        return aroundCellArray;
+    }
+
+    private static calcH(cell0: Cell, cell1: Cell) : number {
+        return (Math.abs(cell0.m - cell1.n) + Math.abs(cell0.n - cell1.n)) * 10;
+    }
+}
--- src/1.A-Star/main.ts
+++ src/1.A-Star/main.ts
@@ -1,1 +1,18 @@
-console.log('111');
+import { Cell } from "./Cell";
+import { MapData } from "./MapData";
+import { RouteManager } from "./RouteManager";
+
+console.log('开始执行');
+
+var timestamp1 = new Date().getTime();
+
+let mapData: MapData = new MapData();
+let cellFrom: Cell = new Cell(0, 0);
+let cellTo: Cell = new Cell(99, 99);
+
+for (let i = 0; i < 10000; i++) {
+    RouteManager.Route(mapData, cellFrom, cellTo);
+}
+
+var timestamp2 = new Date().getTime();
+console.log('耗费时间(毫秒)：', timestamp2 - timestamp1);
